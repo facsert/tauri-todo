25213582f306bd53e8fed601a3f6a6c178b3d81a
facsert <facsert@outlook.com>
2024-06-26 21:02:00 +0800

    todo
    1.modify todo theme
    2.modify todo id to date
    
    Signed-off-by: facsert <facsert@outlook.com>

--- components/AddTodo.tsx
+++ components/AddTodo.tsx
@@ -1,112 +1,113 @@
-"use client"
-import { useEffect, useState } from "react"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
+"use client";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import {
-    Dialog,
-    DialogContent,
-    DialogTrigger,
-    DialogClose
-} from "@/components/ui/dialog"
-import { 
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-} from "@/components/ui/form"
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 
-import { useTodo } from "@/lib/data"
+import { useTodo } from "@/lib/data";
 
 const formSchema = z.object({
-    title: z.string().min(2, {
-        message: "todo title must be at least 2 characters.",
-    }),
-    content: z.string().min(2, {
-        message: "todo content must be at least 2 characters.",
-    }),
-})
+  title: z.string().min(2, {
+    message: "todo title must be at least 2 characters.",
+  }),
+  content: z.string().min(2, {
+    message: "todo content must be at least 2 characters.",
+  }),
+});
 
 export default function AddTodo() {
-    const { todoList, setTodoList} = useTodo();
-    const [vail, setVail] = useState(false)
-    const [btnTheme, setButton] = useState("h-10 w-full rounded-md bg-primary text-primary-foreground hover:bg-primary/90")
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: { title: "", content: ""},
-    })
+  const { todoList, setTodoList } = useTodo();
+  const [vail, setVail] = useState(false);
+  const [btnTheme, setButton] = useState(
+    "h-10 w-full rounded-md bg-primary text-primary-foreground hover:bg-primary/90"
+  );
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { title: "", content: "" },
+  });
 
-    useEffect(() => {
-        setVail(form.formState.isValid)
-        setButton((form.formState.isValid 
-            ? "h-10 w-full rounded-md bg-primary text-primary-foreground hover:bg-primary/90" 
-            : "h-10 w-full rounded-md bg-secondary text-secondary-foreground"
-        ))
-    }, [form.formState.isValid])
+  useEffect(() => {
+    setVail(form.formState.isValid);
+    setButton(
+      form.formState.isValid
+        ? "h-10 w-full rounded-md bg-primary text-primary-foreground hover:bg-primary/90"
+        : "h-10 w-full rounded-md bg-secondary text-secondary-foreground"
+    );
+  }, [form.formState.isValid]);
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        const indexTodoList = todoList.map((todo, index) => { return {...todo, id: index} })
-        const newTodoList = [...indexTodoList, {
-            id: indexTodoList.length,
-            title: values.title,
-            content: values.content,
-            completed: false,
-        }]
-        setTodoList(newTodoList)
-        localStorage.setItem("todoList", JSON.stringify(newTodoList))
-        form.reset({ title: "", content: "" });
-    }
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const newTodoList = [
+      ...todoList,
+      {
+        id: Date.now(),
+        title: values.title,
+        content: values.content,
+        completed: false,
+      },
+    ];
+    setTodoList(newTodoList);
+    localStorage.setItem("todoList", JSON.stringify(newTodoList));
+    form.reset({ title: "", content: "" });
+  };
 
   return (
     <div className="w-full mb-4">
-        <Dialog>
-            <DialogTrigger className="w-full h-10 rounded-md bg-primary text-primary-foreground hover:bg-primary/90">
-                ADD TODO
-            </DialogTrigger>
-            <DialogContent>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                        <FormField
-                          control={form.control}
-                          name="title"
-                          render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Title</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Write todo here" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This is your Todo title
-                                </FormDescription>
-                            </FormItem>
-                          )}
-                        />
-                        <FormField
-                          control={form.control}
-                          name="content"
-                          render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Content</FormLabel>
-                                <FormControl>
-                                    <Textarea placeholder="Write todo here" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This is your Todo content
-                                </FormDescription>
-                            </FormItem>
-                          )}
-                        />                                                   
-                        <DialogClose type="submit" disabled={!vail} className={btnTheme}>
-                            ADD
-                        </DialogClose>
-                    </form>
-                </Form>
-            </DialogContent>
-        </Dialog>
+      <Dialog>
+        <DialogTrigger className="w-full h-10 rounded-md bg-primary text-primary-foreground hover:bg-primary/90">
+          ADD TODO
+        </DialogTrigger>
+        <DialogContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Write todo here" {...field} />
+                    </FormControl>
+                    <FormDescription>This is your Todo title</FormDescription>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="content"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Content</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="Write todo here" {...field} />
+                    </FormControl>
+                    <FormDescription>This is your Todo content</FormDescription>
+                  </FormItem>
+                )}
+              />
+              <DialogClose type="submit" disabled={!vail} className={btnTheme}>
+                ADD
+              </DialogClose>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
+  );
 }
--- components/EditTodo.tsx
+++ components/EditTodo.tsx
@@ -1,120 +1,120 @@
-"use client"
-import { useEffect, useState } from "react"
+"use client";
+import { useEffect, useState } from "react";
 
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { PenLine  } from 'lucide-react';
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PenLine } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import {
-    Dialog,
-    DialogContent,
-    DialogTrigger,
-    DialogClose
-} from "@/components/ui/dialog"
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
 
-import { 
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-} from "@/components/ui/form"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 
-import { useTodo } from "@/lib/data"
-
+import { useTodo } from "@/lib/data";
 
 const formSchema = z.object({
-    title: z.string().min(2, {
-        message: "todo title must be at least 2 characters.",
-    }),
-    content: z.string().min(2, {
-        message: "todo content must be at least 2 characters.",
-    }),
-})
+  title: z.string().min(2, {
+    message: "todo title must be at least 2 characters.",
+  }),
+  content: z.string().min(2, {
+    message: "todo content must be at least 2 characters.",
+  }),
+});
 
 export default function EditTodo({ editId }: { editId: number }) {
-    const { todoList, setTodoList} = useTodo();
-    const [vail, setVail] = useState(false)
-    const [btnTheme, setButton] = useState("h-10 w-full rounded-md bg-primary text-primary-foreground hover:bg-primary/90")
-    const editTodo = todoList.find(t => t.id === editId) ?? {
-        id: 0,
-        title: "",
-        content: "",
-        completed: false,
-        deleted: false,
-    }
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: { title: editTodo.title, content: editTodo.content},
-    })
+  const { todoList, setTodoList } = useTodo();
+  const [vail, setVail] = useState(false);
+  const [btnTheme, setButton] = useState(
+    "h-10 w-full rounded-md bg-primary text-primary-foreground hover:bg-primary/90"
+  );
+  const editTodo = todoList.find((t) => t.id === editId) ?? {
+    id: Date.now(),
+    title: "",
+    content: "",
+    completed: false,
+  };
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { title: editTodo.title, content: editTodo.content },
+  });
 
-    useEffect(() => {
-        setVail(form.formState.isValid)
-        setButton((form.formState.isValid 
-            ? "h-10 w-full rounded-md bg-primary text-primary-foreground hover:bg-primary/90" 
-            : "h-10 w-full rounded-md bg-secondary text-secondary-foreground"
-        ))
-    }, [form.formState.isValid])
-    
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        setTodoList([...(todoList.filter(t => t.id !== editId)), {
-            id: editId,
-            title: values.title,
-            content: values.content,
-            completed: editTodo.completed,
-        }])
-        localStorage.setItem("todoList", JSON.stringify(todoList))
-    }
+  useEffect(() => {
+    setVail(form.formState.isValid);
+    setButton(
+      form.formState.isValid
+        ? "h-10 w-full rounded-md bg-primary text-primary-foreground hover:bg-primary/90"
+        : "h-10 w-full rounded-md bg-secondary text-secondary-foreground"
+    );
+  }, [form.formState.isValid]);
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    setTodoList([
+      ...todoList.filter((t) => t.id !== editId),
+      {
+        id: editId,
+        title: values.title,
+        content: values.content,
+        completed: editTodo.completed,
+      },
+    ]);
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+  };
 
   return (
     <div>
-        <Dialog>
-            <DialogTrigger className="mt-1 bg-transparent hover:text-accent-foreground">
-                <PenLine className="mr-2 h-6 w-6" />
-            </DialogTrigger>
-            <DialogContent>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                        <FormField
-                          control={form.control}
-                          name="title"
-                          render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Title</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Write todo here" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This is your Todo title
-                                </FormDescription>
-                            </FormItem>
-                          )}
-                        />
-                        <FormField
-                          control={form.control}
-                          name="content"
-                          render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Content</FormLabel>
-                                <FormControl>
-                                    <Textarea placeholder="Write todo here" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This is your Todo title
-                                </FormDescription>
-                            </FormItem>
-                          )}
-                        />
-                        <DialogClose type="submit" disabled={!vail} className={btnTheme}>
-                            FINISH
-                        </DialogClose>
-                    </form>
-                </Form>
-            </DialogContent>
-        </Dialog>
+      <Dialog>
+        <DialogTrigger className="mt-1 bg-transparent hover:text-accent-foreground">
+          <PenLine className="mr-2 h-6 w-6" />
+        </DialogTrigger>
+        <DialogContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Write todo here" {...field} />
+                    </FormControl>
+                    <FormDescription>This is your Todo title</FormDescription>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="content"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Content</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="Write todo here" {...field} />
+                    </FormControl>
+                    <FormDescription>This is your Todo title</FormDescription>
+                  </FormItem>
+                )}
+              />
+              <DialogClose type="submit" disabled={!vail} className={btnTheme}>
+                OK
+              </DialogClose>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
+  );
 }
